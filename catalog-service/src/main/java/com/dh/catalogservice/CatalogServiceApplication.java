403262436ba6df16d3bdc51101d022556f071a13
Stefany  Salamanca <stefany9209@gmail.com>
2023-03-13 01:50:28 -0500

    primera versión estable

--- catalog-service/src/main/java/com/dh/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/dh/catalogservice/CatalogServiceApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
